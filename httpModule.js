e4047c845ced303de348abedb179e5618033e244
Saad Srabon <saadsrabon2@gmail.com>
2025-05-14 05:18:16 +0600

    http module enhanced

--- httpModule.js
+++ httpModule.js
@@ -1,1 +1,11 @@
 const http = require('http');
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(' <div><form action="http://localhost:3000" method="POST"><input type="text" name="name" placeholder="Enter your name"><input type="submit" value="Submit"></form></div>');
+    res.end();
+});
+
+//html form
+
+server.listen(3000)

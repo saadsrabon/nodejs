e18288ac3447dd9a0702134c3eca58d5d98c2256
Saad Srabon <saadsrabon2@gmail.com>
2025-05-16 18:38:09 +0600

    random files added

--- check.js
+++
@@ -1,6 +0,0 @@
-const a =20; //IFFE
- const b =(() => {
-  return 20;
-}) 
-module.exports ={a,b,module}; // export default
-
--- index.js
+++
@@ -1,9 +0,0 @@
-//path
-//fs
-//os
-const fs = require('fs');
-const os = require('os');
-
-const user = ('OS Type:', os.totalmem()	);
-
-console.log(user);
